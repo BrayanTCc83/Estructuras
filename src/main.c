27e8df2a5bbb62aa85ca4f8c07226d12d5c976a9
Brayan <btellez948@gmail.com>
2025-04-18 01:58:24 -0600

    [Feature] Example of list use.

--- src/main.c
+++ src/main.c
@@ -2,39 +2,39 @@
 #include <stdlib.h>
 #include "headers/static_stack.h"
 #include "headers/static_queue.h"
+#include "headers/static_list.h"
 
 int main() {
-    /*StaticStack *sstack = new_static_stack(10);
-    printf("Stack en %p\n", sstack);
+    StaticList *list = new_static_list(10);
+    static_list_push(list, 10);
+    static_list_push(list, 9);
+    static_list_shift(list, 8);
+    static_list_shift(list, 7);
+    static_list_push(list, 10);
+    static_list_push(list, 10);
 
-    static_stack_push(sstack, 10);
-    static_stack_push(sstack, 15);
+    int result = static_list_unshift(list);
+    printf("Value: %d\n", result);
+    result = static_list_pull(list);
+    printf("Value: %d\n", result);
+    
+    printf("Index of 10: %d\n", static_list_get_last_index(*list, 10));
+    printf("Index of 3: %d\n", static_list_get_last_index(*list, 3));
+    printf("Index of 10: %d\n", static_list_get_first_index(*list, 10));
+    printf("Index of 3: %d\n", static_list_get_first_index(*list, 3));
+    
+    static_list_insert_after(list, 5, 9);
+    static_list_insert_before(list, 5, 9);
+    static_list_remove_first(list, 10);
+    static_list_remove_last(list, 10);
+    static_list_shift(list, 8);
+    static_list_shift(list, 8);
+    static_list_shift(list, 8);
+    printf("Se eliminaron %d valores.\n", static_list_remove_all(list, 5));
+    printf("Hay %d repeticiones del numero 8.\n", static_list_count(*list, 8));
 
-    int result;
-    while(result != ERROR_VALUE) {
-        result = static_stack_pop(sstack);
-        printf("Valor pop: %d\n", result);
-    }
-    printf("Stack vacio.\n");
-
-    delete_static_stack(sstack);
-    sstack = NULL;
-    printf("Fin del programa\n");*/
-    StaticQueue *squeue = new_static_queue(10);
-    for(int i = 1; i <= 10; i++) {
-        static_queue_enqueue(squeue, i * 2);
-    }
-    printf("Cola llena\n");
-
-    int result = static_queue_dequeue(squeue);
-    while(result != ERROR_VALUE) {
-        //printf("Valor deseconlado: %d\n", result);
-        result = static_queue_dequeue(squeue);
-    }
-    printf("La Queue esta vacia.\n");
-
-    delete_static_queue(squeue);
-    squeue = NULL;
-
+    delete_static_list(list);
+    list = NULL;
+    
     return EXIT_SUCCESS;
 }
