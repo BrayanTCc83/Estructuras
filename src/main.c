b0aaf83d6c12bf6369bf83f8b0d94ab460d91850
Brayan <btellez948@gmail.com>
2025-04-18 02:51:53 -0600

    [Fix] Static Sorted List case use in main

--- src/main.c
+++ src/main.c
@@ -3,38 +3,24 @@
 #include "headers/static_stack.h"
 #include "headers/static_queue.h"
 #include "headers/static_list.h"
+#include "headers/static_sorted_list.h"
 
 int main() {
-    StaticList *list = new_static_list(10);
-    static_list_push(list, 10);
-    static_list_push(list, 9);
-    static_list_shift(list, 8);
-    static_list_shift(list, 7);
-    static_list_push(list, 10);
-    static_list_push(list, 10);
+    StaticSortedList *sortedlList = new_static_sorted_list(10, ASCENDING);
 
-    int result = static_list_unshift(list);
-    printf("Value: %d\n", result);
-    result = static_list_pull(list);
-    printf("Value: %d\n", result);
+    static_sorted_list_insert(sortedlList, 5);
+    static_sorted_list_insert(sortedlList, 4);
+    static_sorted_list_insert(sortedlList, 2);
+    static_sorted_list_insert(sortedlList, 10);
     
-    printf("Index of 10: %d\n", static_list_get_last_index(*list, 10));
-    printf("Index of 3: %d\n", static_list_get_last_index(*list, 3));
-    printf("Index of 10: %d\n", static_list_get_first_index(*list, 10));
-    printf("Index of 3: %d\n", static_list_get_first_index(*list, 3));
+    printf("El dato 2, esta en: %d\n", static_sorted_list_get_index(*sortedlList, 2));
+    printf("El dato 1, esta en: %d\n", static_sorted_list_get_index(*sortedlList, 1));
+    printf("El dato 3, esta en: %d\n", static_sorted_list_get_index(*sortedlList, 3));
     
-    static_list_insert_after(list, 5, 9);
-    static_list_insert_before(list, 5, 9);
-    static_list_remove_first(list, 10);
-    static_list_remove_last(list, 10);
-    static_list_shift(list, 8);
-    static_list_shift(list, 8);
-    static_list_shift(list, 8);
-    printf("Se eliminaron %d valores.\n", static_list_remove_all(list, 5));
-    printf("Hay %d repeticiones del numero 8.\n", static_list_count(*list, 8));
+    static_sorted_list_remove(sortedlList, 10);
+    static_sorted_list_reverse(sortedlList);
 
-    delete_static_list(list);
-    list = NULL;
-    
+    delete_static_sorted_list(sortedlList);
+    sortedlList = NULL;
     return EXIT_SUCCESS;
 }
