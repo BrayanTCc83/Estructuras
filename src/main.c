6094b2fb25e413ba030438564691ced7dbcb8426
Brayan <btellez948@gmail.com>
2025-05-02 10:46:24 -0600

    [Feature] Add case of use for circular queue.

--- src/main.c
+++ src/main.c
@@ -4,23 +4,28 @@
 #include "headers/static_queue.h"
 #include "headers/static_list.h"
 #include "headers/static_sorted_list.h"
+#include "headers/static_circular_queue.h"
 
 int main() {
-    StaticSortedList *sortedlList = new_static_sorted_list(10, ASCENDING);
+    StaticCircularQueue *queue = new_static_circular_queue(10);
 
-    static_sorted_list_insert(sortedlList, 5);
-    static_sorted_list_insert(sortedlList, 4);
-    static_sorted_list_insert(sortedlList, 2);
-    static_sorted_list_insert(sortedlList, 10);
+    printf("Queue: %d\n", sizeof(StaticQueue));
+    printf("Circular Queue: %d\n", sizeof(StaticCircularQueue));
     
-    printf("El dato 2, esta en: %d\n", static_sorted_list_get_index(*sortedlList, 2));
-    printf("El dato 1, esta en: %d\n", static_sorted_list_get_index(*sortedlList, 1));
-    printf("El dato 3, esta en: %d\n", static_sorted_list_get_index(*sortedlList, 3));
-    
-    static_sorted_list_remove(sortedlList, 10);
-    static_sorted_list_reverse(sortedlList);
+    for(int i = 1; i < 11; i++) {
+        static_circular_queue_enqueue(queue, i);
+    }
 
-    delete_static_sorted_list(sortedlList);
-    sortedlList = NULL;
+    for(int i = 0; i < 5; i++) {
+        int res = static_circular_queue_dequeue(queue);
+        printf("Dequeue: %d\n", res);
+    }
+    
+    for(int i = 11; i < 16; i++) {
+        static_circular_queue_enqueue(queue, i);
+    }
+
+    delete_static_circular_queue(queue);
+    queue = NULL;
     return EXIT_SUCCESS;
 }
